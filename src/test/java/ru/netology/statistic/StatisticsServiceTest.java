1f4b492e666bbe371117bb29e86864e6f6c27be9
be.sotnikova <bsotnikova@mail.ru>
2022-11-17 22:16:40 +0400

    new pom + spotbugs

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 16, 8, 6, 11, 11, 12};
+        long expected = 16;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
